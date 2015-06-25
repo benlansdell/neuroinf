f39b006e6be0efceca7555d1e236704d3e3e5179
Ben Lansdell <lansdell@uw.edu>
2015-06-24 18:10:35 -0700

    startup.m

---
+++ startup.m
@@ -0,0 +1,16 @@
+if ismac
+        homedir = '/Users/';
+else
+        homedir = '/home/';
+end
+
+addpath([homedir 'lansdell/projects/neuroinf/code']);
+addpath([homedir 'lansdell/projects/neuroinf/data']);
+addpath([homedir 'lansdell/projects/neuroinf/functions']);
+addpath([homedir 'lansdell/projects/neuroinf/eval']);
+addpath([homedir 'lansdell/projects/neuroinf/fitting']);
+addpath([homedir 'lansdell/projects/neuroinf/preprocess']);
+addpath([homedir 'lansdell/projects/neuroinf/models']);
+addpath_recurse([homedir 'lansdell/projects/neuroinf/code']);
+
+

3a31558b335c3ee8bc59ef242671c90dc8855f11
Ben Lansdell <lansdell@uw.edu>
2015-08-14 14:57:30 -0700

    update startup

--- startup.m
+++ startup.m
@@ -1,19 +1,7 @@
-if ismac
-        homedir = '/Users/';
-else
-        homedir = '/home/';
-end
+wd = pwd;
 
-addpath([homedir 'lansdell/projects/neuroinf/code']);
-addpath([homedir 'lansdell/projects/neuroinf/data']);
-addpath([homedir 'lansdell/projects/neuroinf/functions']);
-addpath([homedir 'lansdell/projects/neuroinf/eval']);
-addpath([homedir 'lansdell/projects/neuroinf/fitting']);
-addpath([homedir 'lansdell/projects/neuroinf/preprocess']);
-addpath([homedir 'lansdell/projects/neuroinf/models']);
-addpath([homedir 'lansdell/projects/neuroinf/scripts']);
-addpath_recurse([homedir 'lansdell/projects/neuroinf/code']);
-addpath_recurse([homedir 'lansdell/matlab/L1General']);
-addpath_recurse([homedir 'lansdell/matlab/schmidt']);
-
+addpath([pwd '/data']);
+addpath([pwd '/functions']);
+addpath([pwd '/scripts']);
+addpath_recurse([pwd '/code']);
 

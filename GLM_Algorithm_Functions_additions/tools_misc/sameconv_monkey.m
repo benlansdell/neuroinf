deb00584b033d85ac4c643f86280ad847992eade
Ben Lansdell <lansdell@uw.edu>
2015-08-14 18:24:46 -0700

    Update dir structure

---
+++ GLM_Algorithm_Functions_additions/tools_misc/sameconv_monkey.m
@@ -0,0 +1,20 @@
+function G = sameconv(A, B);
+%  G = sameconv(A, B);
+%   
+%  Causally filters A with B, giving a column vector with same height as
+%  A.  (B not flipped as in standard convolution).
+%
+%  Convolution performed efficiently in (zero-padded) Fourier domain.
+
+
+[am, an] = size(A);
+[bm, bn] = size(B);
+nn = am+bm-1;
+
+G = ifft(sum(fft(A,nn).*fft(flipud(B),nn),2));
+%This will make stim before stim
+%Gb = G(1:am,:);
+%This will make 'stim' after spikes
+%Ga = G(end-am+1:end,:);
+%This will make 'stim' half before, half after spikes
+G = G(ceil(bm/2)+1:ceil(bm/2)+am,:);

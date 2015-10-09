0442d64dd4ccda6af4079d41052a9bac8f799c16
Ben Lansdell <lansdell@uw.edu>
2015-10-09 14:06:28 -0700

    Make startup.m deployment friendly

--- startup.m
+++ startup.m
@@ -1,7 +1,9 @@
 wd = pwd;
-addpath([pwd '/functions']);
-addpath([pwd '/scripts']);
-addpath_recurse([pwd '/GLM_Algorithm_Functions']);
-addpath_recurse(['~/matlab/schmidt/']);
-addpath_recurse(['~/matlab/chronux/']);
-addpath('~/matlab/plot2svg');
+if (~isdeployed)
+	addpath([pwd '/functions']);
+	addpath([pwd '/scripts']);
+	addpath_recurse([pwd '/GLM_Algorithm_Functions']);
+	addpath_recurse(['~/matlab/schmidt/']);
+	addpath_recurse(['~/matlab/chronux/']);
+	addpath('~/matlab/plot2svg');
+end

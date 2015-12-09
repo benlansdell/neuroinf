1e72603779c39c23b0859166eec631cffd8f67fe
Ben Lansdell <lansdell@uw.edu>
2015-12-08 17:57:54 -0800

    Prepare cross validation code

---
+++ MotorScripts/script_303_MotorData_SimCoupledGLM_crossval.m
@@ -0,0 +1,22 @@
+lambdas = [.1 .3 1 3 10 30 100 300];
+wd = '../MotorData/';
+
+%Simulate coupled GLM with range of lambda values
+%This takes a long time, so split into many runs of a smaller number of
+%repetitions to divide between CPUs/computers if necessary. 
+
+%These runs should be moved to the one directory, and combined and analyzed afterwards
+%by collapse_sim_coupled_GLM_L1.m
+for idx = 1:length(lambdas)
+	sim_coupled_GLM_L1_crossval(wd, 1, 83, idx, fold, nfolds);
+	sim_coupled_GLM_L1_crossval(wd, 2, 83, idx, fold, nfolds);
+	sim_coupled_GLM_L1_crossval(wd, 3, 83, idx, fold, nfolds);
+	sim_coupled_GLM_L1_crossval(wd, 4, 83, idx, fold, nfolds);
+	sim_coupled_GLM_L1_crossval(wd, 5, 83, idx, fold, nfolds);
+	sim_coupled_GLM_L1_crossval(wd, 6, 83, idx, fold, nfolds);
+	sim_coupled_GLM_L1_crossval(wd, 7, 83, idx, fold, nfolds);
+	sim_coupled_GLM_L1_crossval(wd, 8, 83, idx, fold, nfolds);
+	sim_coupled_GLM_L1_crossval(wd, 9, 83, idx, fold, nfolds);
+end
+
+%Cross validate simulations by only taking subsamples of data...
